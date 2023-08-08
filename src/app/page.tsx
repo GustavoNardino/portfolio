332f9f080a930049dc626b45ef4c5711e55423e4
gustavonardino <fgustavo.n@gmail.com>
2023-08-08 16:19:49 -0300

    navbar responsiva mobile first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,16 @@ const Home = () => {
           width={300}
           height={300}
           alt="Profile image" />
-        <h4 className={styles.text}>Breve apresentação</h4>
-        <p className={styles.text}>Nascido em Curitiba sempre fui inquieto e curioso. Me encanta saber como tudo funciona por trás dos panos, então o mundo da tecnologia é um campo fértil.</p>
-        <h4 className={styles.text}>Um pouco da minha trajetória</h4>
-        <p className={styles.text}>Há anos sou fascinado por linguagens de programação e outras tecnologias de desenvolvimento. Ainda na adolescência, em 2007, me divertia com BatScript participando de fóruns de discussões no Orkut. Mais tarde trabalhei com Flash, C# e Unity, entre 2011 e 2015. Atualmente contribuo, sob orientação de metodologias ágeis, em projetos como Desenvolvedor Front End, aplicando conceitos de UX/UI e responsividade. Tenho enorme apreço pelo aprendizado. Tenho paixão pelo universo Linux e Desenvolvimento Full stack.</p>
+        <div>
+          <h4 className={styles.text}>Breve apresentação</h4>
+          <p className={styles.text}>Nascido em Curitiba sempre fui inquieto e curioso. Me encanta saber como tudo funciona por trás dos panos, então o mundo da tecnologia é um campo fértil.</p>
+
+        </div>
+        <div>
+          <h4 className={styles.text}>Um pouco da minha trajetória</h4>
+          <p className={styles.text}>Há anos sou fascinado por linguagens de programação e outras tecnologias de desenvolvimento. Ainda na adolescência, em 2007, me divertia com BatScript participando de fóruns de discussões no Orkut. Mais tarde trabalhei com Flash, C# e Unity, entre 2011 e 2015. Atualmente contribuo, sob orientação de metodologias ágeis, em projetos como Desenvolvedor Front End, aplicando conceitos de UX/UI e responsividade. Tenho enorme apreço pelo aprendizado. Tenho paixão pelo universo Linux e Desenvolvimento Full stack.</p>
+
+        </div>
       </div>
     </PagesLayout>
   )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,31 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './navbar.module.css'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+    const [mobileMenu, setMobileMenu] = useState(false)
+
     return (
         <div className={styles.container}>
-            <Link className={styles.menulink} href={'/'}>Apresentação</Link>
-            <Link className={styles.menulink} href={'/experiences'}>Experiências</Link>
-            <Link className={styles.menulink} href={'/labs'}>Laboratório</Link>
+            <div className={styles.mobileMenuButton} onClick={() => mobileMenu ? setMobileMenu(false) : setMobileMenu(true)}>
+                |||
+            </div>
+            {mobileMenu &&
+                <div className={styles.menuDropdown}>
+                    <Link onClick={() => setMobileMenu(false)} className={styles.menulink} href={'/'}>Apresentação</Link>
+                    <Link onClick={() => setMobileMenu(false)} className={styles.menulink} href={'/experiences'}>Experiências</Link>
+                    <Link onClick={() => setMobileMenu(false)} className={styles.menulink} href={'/labs'}>Laboratório</Link>
+                </div>
+            }
+            <div className={styles.menuLinks}>
+                <Link className={styles.menulink} href={'/'}>Apresentação</Link>
+                <Link className={styles.menulink} href={'/experiences'}>Experiências</Link>
+                <Link className={styles.menulink} href={'/labs'}>Laboratório</Link>
+            </div>
         </div>
     )
 }
